04551c82b3df4b4b5a610ea787d2aea306cda2ae
Stefan Walther <stefanwalther@users.noreply.github.com>
2017-05-30 15:04:31 +0200

    chore: Basic version + tests

---
+++ test/test-verb.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+RESTORE='\033[0m'
+
+# Misc colors
+RED='\033[00;31m'
+GREEN='\033[00;32m'
+YELLOW='\033[00;33m'
+BLUE='\033[00;34m'
+PURPLE='\033[00;35m'
+CYAN='\033[00;36m'
+LIGHTGRAY='\033[00;37m'
+
+LRED='\033[01;31m'
+LGREEN='\033[01;32m'
+LYELLOW='\033[01;33m'
+LBLUE='\033[01;34m'
+LPURPLE='\033[01;35m'
+LCYAN='\033[01;36m'
+WHITE='\033[01;37m'
+
+# Icons
+S_CHECK='\u2714'
+S_BULLET='\xE2\x80\xA2'
+
+SPACE='    '
+
+# Todo: Test test-system: https://github.com/sstephenson/bats
+
+cleanup() {
+    rm -rf ./README.md
+}
+
+cd /app/fixtures;
+cleanup;
+verb;
+
+if [ ! -f /app/fixtures/README.md ]; then
+    echo ""
+    echo "${RED}==> Test: README.md not found!" 1>&2${RESTORE};
+    exit 1;
+
+else
+    echo
+    echo "${GREEN}${S_CHECK} Test: README.md successfully created.${RESTORE}"
+fi
+
+# compare files
+comm -2 -3 /app/fixtures/README.md /app/expected/README.md
+
+
+#cleanup;
+
+echo "==> tests successfully finished";
--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-docker build -t stefanwalther/verb ./../;
+docker build -t stefanwalther/docker-verb ./../;
 
-docker run -it stefanwalther/verb /bin/bash;
+docker run \
+    -it \
+    --privileged \
+    -v ${PWD}/fixtures:/app/fixtures \
+    -v ${PWD}/expected:/app/expected \
+    -v ${PWD}/test-verb.sh:/test-verb.sh \
+    stefanwalther/docker-verb \
+    /bin/sh -c \
+    "sh /test-verb.sh";
